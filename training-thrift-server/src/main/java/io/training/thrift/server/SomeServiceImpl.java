0be65ad286a15b193697c7154707c8416d471178
Percy0601 <2008dreams@2008.sina.com>
2021-06-06 00:04:02 +0800

    可以正常启动

--- training-thrift-server/src/main/java/io/training/thrift/server/SomeServiceImpl.java
+++ training-thrift-server/src/main/java/io/training/thrift/server/SomeServiceImpl.java
@@ -2,11 +2,14 @@ package io.training.thrift.server;
 
 import io.training.thrift.api.SomeService;
 import io.training.thrift.api.User;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.thrift.TException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+@Slf4j
 public class SomeServiceImpl implements SomeService.Iface {
     @Override
     public String echo(String msg) throws TException {
@@ -15,13 +18,18 @@ public class SomeServiceImpl implements SomeService.Iface {
 
     @Override
     public int addUser(User user) throws TException {
-
         user.setUserId(new Random().nextInt(100));
+        log.info("request addUser: user:{}", user);
         return user.getUserId();
     }
 
     @Override
     public List<User> findUserByIds(List<Integer> idList) throws TException {
-        return null;
+        List<User> users = new ArrayList<>();
+        User user = new User();
+        user.setUserId(new Random().nextInt(1000));
+        users.add(user);
+        log.info("request findUserByIds, idList:{}, result:{}", idList, users);
+        return users;
     }
 }
