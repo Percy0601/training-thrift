4c6e5a9a20df2d2478b31db628f655ddc95f9bc9
Percy0601 <2008dreams@2008.sina.com>
2021-07-30 00:12:34 +0800

    临时提交

--- training-thrift-client/src/main/java/io/training/thrift/client/ClientBootstrap.java
+++ training-thrift-client/src/main/java/io/training/thrift/client/ClientBootstrap.java
@@ -4,7 +4,9 @@ import io.training.thrift.api.SomeService;
 import io.training.thrift.extension.AttachableBinaryProtocol;
 import org.apache.thrift.TException;
 import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TMultiplexedProtocol;
 import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TFramedTransport;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.TTransportException;
@@ -18,7 +20,7 @@ import java.util.UUID;
 public class ClientBootstrap {
     private static Logger log = LoggerFactory.getLogger(ClientBootstrap.class);
     public static void main(String[] args) {
-        handleAttachment();
+        handleMultiProtocol();
     }
 
     private static void standard() {
@@ -43,11 +45,12 @@ public class ClientBootstrap {
         }
     }
 
+
     private static void handleAttachment() {
         System.out.println("客户端启动....");
         TTransport transport = null;
         try {
-            transport = new TSocket("localhost", 9898, 30000);
+            transport = new TSocket("localhost", 8761, 30000);
             // 协议要和服务端一致
             TProtocol protocol = new AttachableBinaryProtocol(transport);
             SomeService.Client client = new SomeService.Client(protocol);
@@ -75,4 +78,36 @@ public class ClientBootstrap {
             }
         }
     }
+
+    private static void handleMultiProtocol() {
+        System.out.println("客户端启动....");
+        TTransport transport = null;
+        try {
+            String addr = "localhost:8761";
+            String[] ipAndPort = addr.split(":");
+            TSocket tsocket = new TSocket(ipAndPort[0], Integer.valueOf(ipAndPort[1]));
+            transport = new TFramedTransport(tsocket);
+            TProtocol protocol = new TBinaryProtocol(transport);
+
+            TMultiplexedProtocol multiplexedProtocol = new TMultiplexedProtocol(protocol, SomeService.class.getName() + "$");
+            SomeService.Client client = new SomeService.Client(multiplexedProtocol);
+            transport.open();
+            for(int i = 0; i < 10000; i++) {
+                String hello = client.echo("kdfasdf" + i);
+                System.out.println("======" + hello);
+            }
+
+        } catch (TTransportException e) {
+            e.printStackTrace();
+        } catch (TException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != transport) {
+                transport.close();
+            }
+        }
+    }
+
+
+
 }
