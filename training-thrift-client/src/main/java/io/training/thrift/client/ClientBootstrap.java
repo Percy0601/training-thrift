ed5b46201f1b4f600685fe4282a5e909d7641091
Percy0601 <2008dreams@2008.sina.com>
2024-05-12 09:42:23 +0800

    断线重连

--- training-thrift-client/src/main/java/io/training/thrift/client/ClientBootstrap.java
+++ training-thrift-client/src/main/java/io/training/thrift/client/ClientBootstrap.java
@@ -21,7 +21,9 @@ import java.util.UUID;
 public class ClientBootstrap {
     private static Logger log = LoggerFactory.getLogger(ClientBootstrap.class);
     public static void main(String[] args) {
-        handleMultiProtocol();
+//        handleMultiProtocol();
+        //standard();
+        handleAttachment();
     }
 
     private static void standard() {
@@ -31,7 +33,9 @@ public class ClientBootstrap {
             transport = new TSocket("localhost", 9898, 30000);
             // 协议要和服务端一致
             TProtocol protocol = new TBinaryProtocol(transport);
-            SomeService.Client client = new SomeService.Client(protocol);
+//            SomeService.Client client = new SomeService.Client(protocol);
+            SomeService.Client.Factory clientFactory = new SomeService.Client.Factory();
+            SomeService.Client client = clientFactory.getClient(protocol);
             transport.open();
             String result = client.echo("哈哈");
             System.out.println(result);
@@ -51,31 +55,76 @@ public class ClientBootstrap {
         System.out.println("客户端启动....");
         TTransport transport = null;
         try {
-            transport = new TSocket("localhost", 8761, 30000);
+            transport = new TSocket("localhost", 9898, 30000);
             // 协议要和服务端一致
             TProtocol protocol = new AttachableBinaryProtocol(transport);
-            SomeService.Client client = new SomeService.Client(protocol);
+//            SomeService.Client client = new SomeService.Client(protocol);
+            SomeService.Client.Factory clientFactory = new SomeService.Client.Factory();
+            SomeService.Client client = clientFactory.getClient(protocol);
             transport.open();
             String result = client.echo("哈哈");
             System.out.println(result);
 
-            List<String> msgList = new ArrayList<>();
+//            List<String> msgList = new ArrayList<>();
+//            for(int i = 0; i < 100000; i++) {
+//                msgList.add(UUID.randomUUID().toString());
+//            }
+//            for(int i = 0; i < 100000; i++) {
+//                result = client.echo(msgList.get(i));
+//                if(!result.equals("Hello " + msgList.get(i))) {
+//                    System.out.println("不相同：" + msgList.get(i));
+//                }
+//            }
+
+
             for(int i = 0; i < 100000; i++) {
-                msgList.add(UUID.randomUUID().toString());
-            }
-            for(int i = 0; i < 100000; i++) {
-                result = client.echo(msgList.get(i));
-                if(!result.equals("Hello " + msgList.get(i))) {
-                    System.out.println("不相同：" + msgList.get(i));
+                String r = UUID.randomUUID().toString();
+                try {
+                    result = client.echo(r);
+                    if(!result.equals("Hello " + r)) {
+                        log.info("不相同：request:{}, result:{}", r ,result);
+                    }
+                } catch (Exception e) {
+                    log.warn("##################:{}", e.getMessage());
+
+                    if (e instanceof TTransportException) {
+                        TTransportException cause = (TTransportException) e;
+                        if (cause.getType() == TTransportException.NOT_OPEN ||
+                                cause.getType() == TTransportException.END_OF_FILE ||
+                                cause.getType() == TTransportException.TIMED_OUT ||
+                                cause.getType() == TTransportException.UNKNOWN) {
+
+                            transport.close();
+                            try {
+                                transport.open();
+                            } catch (TTransportException e1) {
+                                // throw new RuntimeException(e1);
+                            }
+                        }
+                    }
                 }
+                try {
+                    Thread.sleep(1000L);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
             }
         } catch (TTransportException e) {
             e.printStackTrace();
         } catch (TException e) {
             e.printStackTrace();
         } finally {
-            if (null != transport) {
-                transport.close();
+//            if (null != transport) {
+//                transport.close();
+//            }
+
+            if(!transport.isOpen()) {
+                try {
+                    transport.open();
+                } catch (TTransportException e) {
+                    throw new RuntimeException(e);
+                }
             }
         }
     }
@@ -83,10 +132,11 @@ public class ClientBootstrap {
     private static void handleMultiProtocol() {
         System.out.println("客户端启动....");
         TTransport transport = null;
+        TTransport tsocket = null;
         try {
-            String addr = "localhost:8761";
+            String addr = "localhost:9898";
             String[] ipAndPort = addr.split(":");
-            TTransport tsocket = new TSocket(ipAndPort[0], Integer.valueOf(ipAndPort[1]));
+            tsocket = new TSocket(ipAndPort[0], Integer.valueOf(ipAndPort[1]));
             transport = new TFramedTransport(tsocket);
             TProtocol protocol = new TTraceClientProtocol(transport);
 //            TProtocol protocol = new TBinaryProtocol(transport);
